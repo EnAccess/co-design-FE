53af276584b6da3f8689120c0ac28e830845c682
Delyc <d.twizeyima@alustudent.com>
2023-11-08 12:40:25 +0200

    parsing csv to json

--- src/components/GroupedData.jsx
+++ src/components/GroupedData.jsx
@@ -1,18 +1,40 @@
-import classNames from "classnames";
 import React, { useEffect, useState } from "react";
-
-const rootStyle = { display: "flex", justifyContent: "center" };
-const rowStyle = {
-  margin: "200px 0",
-  display: "flex",
-  justifyContent: "space-between",
-};
-const boxStyle = { padding: "10px", border: "1px solid black" };
+import { ArcherContainer, ArcherElement } from "react-archer";
+import csvtojson from 'csvtojson';
 
 function GroupedData() {
   const [data, setData] = useState([]);
-  const [filterLevel, setFilterLevel] = useState(null);
   const [hoveredIndex, setHoveredIndex] = useState(null);
+  const [jsonData, setJsonData] = useState([]);
+
+  useEffect(() => {
+    const csvFilePath = './data.csv';
+    async function fetchData() {
+
+      try {
+        const response = await fetch(csvFilePath);
+        const csvData = await response.text();
+
+        // Parse CSV to JSON
+        const jsonArray = csvData.split('\n').map((line) => {
+          const [Key, Title, Author] = line.split(',');
+          return {
+            Key,
+            Title,
+            Author,
+          };
+        });
+
+        setJsonData(jsonArray);
+        console.log("Json data", jsonData)
+
+      } catch (error) {
+        console.error('Error fetching or parsing CSV:', error);
+      }
+    }
+
+    fetchData();
+  }, []);
 
   const getData = () => {
     fetch("./output.json", {
@@ -22,14 +44,11 @@ function GroupedData() {
       },
     })
       .then(function (response) {
-        console.log(response);
         return response.json();
       })
       .then(function (data) {
         setData(data);
       });
-
-    console.log("dataaaa", data);
   };
 
   useEffect(() => {
@@ -40,60 +59,59 @@ function GroupedData() {
     .map((item, index) => item.PARSED_MANUAL_TAGS.THEME)
     .flat();
   const uniqueThemes = Array.from(new Set(getAllThemes.filter((v) => v)));
-  console.log("uniqueThemes", uniqueThemes);
-
-  const coDesignLevelColors = {
-    1: "bg-red-500",
-    2: "bg-blue-500",
-    3: "bg-green-500",
-  };
-
-  const handleFilter = (level) => {
-    setFilterLevel(level);
-  };
 
   return (
     <section className="mt-10">
-    <h3 className="text-gray-600 font-semibold text-center text-2xl">Core concept of collaborative Design Process</h3>
-      <div className="grid grid-cols-2 gap-5 p-20 w-full">
-        {uniqueThemes.map((item, index) => {
-          return (
-              <div
-              key={index}
-                className={classNames({
-                  "bg-gray-100- border border-dashed p-5 flex flex-col gap-3 bg-gray-100 ": true,
-    "col-span-3 bg-red-500" : index === 5,
+      <h3 className="text-gray-600 font-semibold text-center text-2xl">
+        Core concept of collaborative Design Process
+      </h3>
 
-                })}
-                // className={`bg-gray-100- border border-dashed ${
-                //   index === 0 ? "bg-red-500" : index===1 ? "bg-blue-500" : index===2 ? "bg-green-500" : ""
-                // }`}
-              >
+      <div className="grid grid-cols-2 gap-5 p-20 w-full">
+        <ArcherContainer strokeColor="gray">
+          {uniqueThemes.map((item, index) => {
+            return (
+              <div key={index}>
                 {item}
 
-{data.map((itemdata, dataIndex) => {
-                if (itemdata.PARSED_MANUAL_TAGS.THEME === item) {
-                  return (
-                    <div
-                      key={dataIndex}
-                      className="border border-dashed py-4 px-2 relative"
-                      onMouseEnter={() => setHoveredIndex(dataIndex)}
-                      onMouseLeave={() => setHoveredIndex(null)}>
-                      {itemdata.Title}
-                      {hoveredIndex === dataIndex && (
-                        <div className="absolute bg-white border p-4 mt-2 z-10">
-                          <p>Extra: {itemdata.Extra}</p>
-                          <p>{itemdata.Manual_Tags}</p>
+                <div className="bg-gray-100 border border-dashed p-5 grid grid-cols-3 gap-20">
+                  {data.map((itemdata, dataIndex) => {
+                    if (itemdata.PARSED_MANUAL_TAGS.THEME === item) {
+                      const level = itemdata.PARSED_MANUAL_TAGS.CO_DESIGN_LEVEL;
+                      const backgroundColorClass = `bg-${
+                        level == 1
+                          ? "red-500"
+                          : level == 2
+                          ? "blue-500"
+                          : level == 3
+                          ? "green-500"
+                          : "gray-500"
+                      }`;
+
+                      return (
+                        <div
+                          key={dataIndex}
+                          className={`border border-dashed py-4 px-2 relative ${backgroundColorClass}`}
+                          onMouseEnter={() => setHoveredIndex(dataIndex)}
+                          onMouseLeave={() => setHoveredIndex(null)}
+                        >
+                          {itemdata.Title}
+                          <p>CO_DESIGN_LEVEL: {level} </p>
+
+                          {hoveredIndex === dataIndex && (
+                            <div className="absolute bg-white border p-4 mt-2 z-10">
+                              <p>Extra: {itemdata.Extra}</p>
+                              <p>{itemdata.Manual_Tags}</p>
+                            </div>
+                          )}
                         </div>
-                      )}
-                    </div>
-                  );
-                }
-              })}
-              
-            </div>
-          );
-        })}
+                      );
+                    }
+                  })}
+                </div>
+              </div>
+            );
+          })}
+        </ArcherContainer>
       </div>
     </section>
   );

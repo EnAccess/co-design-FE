bc5d913062b928c40b0adfc44994741bd03bb66e
Delyc <d.twizeyima@alustudent.com>
2023-12-11 09:06:23 +0200

    feat: UI improvement

---
+++ src/components/icons/Line.jsx
@@ -0,0 +1,9 @@
+export default function Line(){
+    return(
+        <svg width="80px" height="80px" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+<g id="Interface / Line_L">
+<path id="Vector" d="M12 19V5" stroke="#dc2626" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+</g>
+</svg>
+    )
+}

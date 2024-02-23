7d591eb838a5750d4e8a8ae9160af8bb8d93d8f1
Thierry N <thierryn@MacBook-Pro-von-Thierry.local>
2024-02-23 15:20:02 +0200

    feat: migrating from CRA to NextJs

--- src/components/Data.jsx
+++ src/components/Data.jsx
@@ -1,4 +1,6 @@
-import React, { useEffect, useState } from "react";
+"use client";
+
+import { useEffect, useState } from "react";
 import { ArcherContainer, ArcherElement } from "react-archer";
 const rootStyle = { display: "flex", justifyContent: "center" };
 const rowStyle = {

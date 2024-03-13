99b37810bf672d381978c83e3006be3146c2316e
Nestor Ngabonziza <ngabonest@gmail.com>
2024-03-13 11:27:33 +0200

    refactor: putting every header sections in their own components

---
+++ src/components/header/index.tsx
@@ -0,0 +1,27 @@
+import React from "react";
+import Image from "next/image";
+import ResourceSection from "./ResourceSection";
+import PracticeSection from "./PracticeSection";
+import JourneySection from "./JourneySection";
+
+export const Header = () => {
+  return (
+    <div className="flex flex-wrap justify-between items-center  gap-y-24 border-b-8 border-dashed px-8 mx-auto">
+      <ResourceSection />
+      <Image
+        src="/images/smallArrow.png"
+        alt="arrow"
+        width={100}
+        height={100}
+      />
+      <PracticeSection />
+      <Image
+        src="/images/smallArrow.png"
+        alt="arrow"
+        width={100}
+        height={100}
+      />
+      <JourneySection />
+    </div>
+  );
+};

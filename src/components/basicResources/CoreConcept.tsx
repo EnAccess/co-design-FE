59ae8355981ef75840b5f3f49fbdcc00e392faf2
Adeline Ayinkamiye <adelinemug6@gmail.com>
2024-03-13 16:16:39 +0200

    refactor: add array of data-item and fix responsiveness

--- src/components/basicResources/CoreConcept.tsx
+++ src/components/basicResources/CoreConcept.tsx
@@ -5,6 +5,29 @@ import YellowCard from "../cards/YellowCard";
 import { ArcherContainer } from "react-archer";
 
 const CoreConcept = () => {
+  const dataItem = [
+    {
+      title: "Systems Approaches to Energy Access",
+      description:
+        "Engaging with entire systems approaches requires humanitarian practitioners to challenge and rethink how to interact with humanitarian systems that surround them. This outlines what needs to change and how.",
+    },
+    {
+      title: "Scale & Replicability",
+      description:
+        "Whilst the idea of replicability (the ability to copy and paste using the same resources) is embedded in many humanitarian organisations, true scalability is often reserved for the private sector - how can the two learn from each other to create more effective and efficient humanitarian programs?",
+    },
+    {
+      title: "Localisation",
+      description:
+        "Formally recognised by the Grand Bargain, localisation engages local and national actors in all phases of humanitarian action. This theme supports localisation processes which include the voices of the forcibly displaced in leading in the sector.",
+    },
+    {
+      title: "Decolonisation",
+      description:
+        "Decolonisation reflects a just transition to rebalance power among the various stakeholders in the humanitarian sector. The theme looks at the stages of transformation in power imbalances, decolonising aid and highlights the need for systematic changes in how the humanitarian sector operates",
+    },
+  ];
+
   return (
     <>
       <div className="flex text-gray-500 justify-center items-center py-6 text-3xl font-semibold">
@@ -14,11 +37,8 @@ const CoreConcept = () => {
       <div className="flex gap-4 flex-col lg:flex-row md:max-w-full h-full">
         <div className="border-4 border-dashed md:w-1/2">
           <YellowCard
-            title={`Systems Approaches
-                to Energy Access`}
-            description={
-              "Engaging with entire systems approaches requires humanitarian practitioners to challenge and rethink how to interact with humanitarian systems that surround them. This outlines what needs to change and how."
-            }
+            title={dataItem[0].title}
+            description={dataItem[0].description}
             className="w-3/4 mt-2 ml-2"
           />
           <div className="float-end mr-44 mb-4 mt-10 w-48">
@@ -28,14 +48,12 @@ const CoreConcept = () => {
           </div>
         </div>
 
-        <div className="md:w-1/2">
+        <div className="lg:w-1/2">
           <div className="flex flex-col md:flex-row gap-4">
             <div className="border-4 border-dashed">
               <YellowCard
-                title={`Scale & Replicability`}
-                description={
-                  "Whilst the idea of replicability (the ability to copy and paste using the same resources) is embedded in many humanitarian organisations, true scalability is often reserved for the private sector - how can the two learn from each other to create more effective and efficient humanitarian programs?"
-                }
+                title={dataItem[1].title}
+                description={dataItem[1].description}
               />
               <div className="w-96 mt-10 float-end mr-12 mb-4">
                 <ArcherContainer>
@@ -45,9 +63,8 @@ const CoreConcept = () => {
             </div>
             <div className="border-4 border-dashed">
               <YellowCard
-                title={`Localisation`}
-                description={"Formally recognised by the Grand Bargain, localisation engages local and national actors in all phases of humanitarian action. This theme supports localisation processes which include the voices of the forcibly displaced in leading in the sector."
-                }
+                title={dataItem[2].title}
+                description={dataItem[2].description}
               />
               <div className="flex ml-3 mt-28 mr-24 gap-3 mb-12 w-96">
                 <div>
@@ -63,17 +80,15 @@ const CoreConcept = () => {
               </div>
             </div>
           </div>
-          <div className="border-4 border-dashed mt-6 flex w-full">
+          <div className="border-4 border-dashed mt-6 flex flex-wrap lg:flex-row">
             <div className="py-2">
               <YellowCard
-                title={`Decolonisation`}
-                description={
-                  "Decolonisation reflects a just transition to rebalance power among the various stakeholders in the humanitarian sector. The theme looks at the stages of transformation in power imbalances, decolonising aid and highlights the need for systematic changes in how the humanitarian sector operates"
-                }
+                title={dataItem[3].title}
+                description={dataItem[3].description}
                 className="flex flex-col gap-4 w-96 ml-3"
               />
             </div>
-            <div className="flex gap-10 mt-2 ml-4">
+            <div className="flex gap-4 mt-2 ml-4">
               <div className="mt-32 lg:w-44">
                 <ArcherContainer>
                   <InformationCard data={Data[101]} />
--- src/components/basicResources/ElementConcept.tsx
+++ src/components/basicResources/ElementConcept.tsx
@@ -5,6 +5,24 @@ import YellowCard from "../cards/YellowCard";
 import { ArcherContainer } from "react-archer";
 
 const ElementConcept = () => {
+  const dataItem = [
+    {
+      title: "Capacity Bulding, Development & Sharing",
+      description:
+        "Capacity building evolves in the co-design spectrum to transform skills and knowledge to foster effective responses for sustainable solutions. It allows humanitarian practitioners to meaningfully engage with communities and provide opportunities to aid transformative solutions.",
+    },
+    {
+      title: "Partnerships",
+      description:
+        "Partnerships, and collaborations between humanitarian organisations, provide the fundamental core of every humanitarian response. This theme tracks the evolution of partnerships from extractive to transformative, shifting decision making power to forcibly displaced groups themselves",
+    },
+    {
+      title: `Gender,
+      Disability & Inclusion`,
+      description:
+        "Inclusion calls for equal participation and representation of vulnerable and marginalised groups despite the differences in ethnicity, gender, disability and identity among others. This theme reflects the importance of inclusive design approaches and the active engagement in creating inclusive policies.",
+    },
+  ];
   return (
     <div className="mt-20">
       <div className="flex text-gray-500 justify-center items-center py-4 text-3xl font-semibold">
@@ -14,10 +32,8 @@ const ElementConcept = () => {
       <div className="flex flex-col md:flex-row gap-3 max-w-full h-full">
         <div className="border-4 border-dashed max-w-[60%]">
           <YellowCard
-            title={`Capacity Bulding, Development & Sharing`}
-            description={
-              "Capacity building evolves in the co-design spectrum to transform skills and knowledge to foster effective responses for sustainable solutions. It allows humanitarian practitioners to meaningfully engage with communities and provide opportunities to aid transformative solutions."
-            }
+            title={dataItem[0].title}
+            description={dataItem[0].description}
             className="w-3/4 mt-2 ml-2"
           />
           <div className="w-44 mt-4 ml-12">
@@ -36,19 +52,14 @@ const ElementConcept = () => {
           <div className="flex gap-4">
             <div className="border-4 border-dashed">
               <YellowCard
-                title={`Partnerships`}
-                description={
-                  "Partnerships, and collaborations between humanitarian organisations, provide the fundamental core of every humanitarian response. This theme tracks the evolution of partnerships from extractive to transformative, shifting decision making power to forcibly displaced groups themselves"
-                }
+                title={dataItem[1].title}
+                description={dataItem[1].description}
               />
             </div>
             <div className="border-4 border-dashed">
               <YellowCard
-                title={`Gender,
-                Disability & Inclusion`}
-                description={
-                  "Inclusion calls for equal participation and representation of vulnerable and marginalised groups despite the differences in ethnicity, gender, disability and identity among others. This theme reflects the importance of inclusive design approaches and the active engagement in creating inclusive policies."
-                }
+                title={dataItem[2].title}
+                description={dataItem[2].description}
               />
               <div className="w-64 ml-6 py-28">
                 <ArcherContainer>

cb20787752df469e8613a572086d1291a1fde2fc
kareraolivier <77101006+kareraolivier@users.noreply.github.com>
2024-03-14 09:55:19 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  basePath: '/co-design-FE',
+  assetPrefix: '/co-design-FE/',
 };
 
 export default nextConfig;

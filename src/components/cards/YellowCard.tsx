f5371714bf2735dd72f328507bca796176c7a0b5
Adeline Ayinkamiye <adelinemug6@gmail.com>
2024-03-14 11:05:26 +0200

    refactor: change background-color

--- src/components/cards/YellowCard.tsx
+++ src/components/cards/YellowCard.tsx
@@ -12,7 +12,7 @@ const YellowCard = ({
   return (
     <>
       <div
-        className={`bg-yellow-200 flex justify-between px-5 py-5 ${className}`}
+        className={`bg-[#ffff99] flex justify-between px-5 py-5 ${className}`}
       >
         <h3 className="text-2xl font-semibold">{title}</h3>
         <p className="text-sm w-3/4 text-center">{description}</p>

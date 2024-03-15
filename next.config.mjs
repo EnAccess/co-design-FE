278c7e901af6d9a151261969b3e342438e90863e
kareraolivier <77101006+kareraolivier@users.noreply.github.com>
2024-03-15 06:39:39 -0700

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
+  reactStrictMode: false,
+  output: "export",
+  images: { unoptimized: true },
 };
 
 export default nextConfig;

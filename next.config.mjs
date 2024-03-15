af8dffb8cf8a222b168b9d42231c317a66c752da
kareraolivier <kareraolivier@gmail.com>
2024-03-15 10:22:04 +0200

    modify next config file

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: "export",
   reactStrictMode: true,
-  basePath: "/co-design-FE",
-  assetPrefix: "/co-design-FE/",
 };
 
-module.exports = nextConfig;
+export default nextConfig;

148c059e4f1b71b68d9439040efb4c8428e40ae2
kareraolivier <kareraolivier@gmail.com>
2024-03-15 16:05:25 +0200

    feat changes needed for deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: false,
+  output: "export",
+  images: {
+    unoptimized: true,
+    loader: "custom",
+    loaderFile: "./my-loader.ts",
+  },
 };
 
 export default nextConfig;

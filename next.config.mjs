a2bba00655353f019b319bd0c7a85dc97410b69a
kareraolivier <kareraolivier@gmail.com>
2024-03-15 08:23:09 +0200

    feat: add the output

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   reactStrictMode: true,
   basePath: "/co-design-FE",
   assetPrefix: "/co-design-FE/",
 };
 
-export default nextConfig;
+module.exports = nextConfig;

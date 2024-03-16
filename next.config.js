0ce38cd7eff14e15e9168561f1e9598e07e9f60e
kareraolivier <kareraolivier@gmail.com>
2024-03-16 11:55:22 +0200

    deployment

---
+++ next.config.js
@@ -0,0 +1,23 @@
+// /** @type {import('next').NextConfig} */
+// const nextConfig = {
+//   reactStrictMode: false,
+//   output: "export",
+//   images: {
+//     unoptimized: true,
+//   },
+// };
+
+// export default nextConfig;
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  reactStrictMode: false,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+};
+
+module.exports = nextConfig;

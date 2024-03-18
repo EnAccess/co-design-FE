c6a6f6e7d70ee310e5b5e850fdcef9a534b8c3e5
kareraolivier <kareraolivier@gmail.com>
2024-03-18 09:46:52 +0200

    feat: add start on component

--- src/utils/Helper.ts
+++ src/utils/Helper.ts
@@ -12,19 +12,32 @@ export const categorizeByCoDesignLevel = (entries: Entries) => {
       const coDesignLevel = entry.PARSED_MANUAL_TAGS.CO_DESIGN_LEVEL;
       switch (coDesignLevel) {
         case "1":
-          acc.level1.push(entry);
+          acc.level1.unshift(entry);
           break;
         case "2":
-          acc.level2.push(entry);
+          acc.level2.unshift(entry);
           break;
         case "3":
-          acc.level3.push(entry);
+          acc.level3.unshift(entry);
           break;
         default:
-          console.log(`Unknown CO_DESIGN_LEVEL: ${coDesignLevel}`);
+          console.error(`Unknown CO_DESIGN_LEVEL: ${coDesignLevel}`);
       }
       return acc;
     },
     { level1: [], level2: [], level3: [] }
   );
 };
+
+export function getColorByAccess(accessType: string) {
+  switch (accessType) {
+    case "Institutional Access":
+      return "#FFFF33"; // Yellow
+    case "Open Source":
+      return "#33FF33"; // green
+    case "Paid Service":
+      return "#33FF33"; // Red
+    default:
+      return "transparent";
+  }
+}

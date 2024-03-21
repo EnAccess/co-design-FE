0230d25fe7edc8bcdc4de8bb7a42ced32a333cad
Adeline Ayinkamiye <adelinemug6@gmail.com>
2024-03-21 15:16:08 +0200

    refactor: change title-card bg-color

--- src/components/cards/TitleCard.tsx
+++ src/components/cards/TitleCard.tsx
@@ -12,10 +12,10 @@ const TitleCard = ({
   return (
     <>
       <div
-        className={`bg-[#ffff66] flex justify-between p-5 ${className}`}
+        className={`bg-[rgb(250,250,131)] flex justify-between p-5 ${className}`}
       >
-        <h3 className="text-3xl text-gray-700 font-bold">{title}</h3>
-        <p className="text-lg text-gray-700 w-3/4 text-center">{description}</p>
+        <h3 className="text-4xl text-gray-700 font-bold">{title}</h3>
+        <p className="text-lg text-gray-700 w-3/4 text-center font-serif">{description}</p>
       </div>
     </>
   );

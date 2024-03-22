fe0ed0c08d264c242ac06862051a863f376f93ab
Adeline Ayinkamiye <adelinemug6@gmail.com>
2024-03-22 09:28:40 +0200

    refactor: change bg-color on title card component

--- src/components/cards/TitleCard.tsx
+++ src/components/cards/TitleCard.tsx
@@ -12,10 +12,10 @@ interface Props {
     return (
       <>
         <div
-          className={`bg-[#ffff66] flex justify-between px-5 py-5 ${className}`}
+          className={`bg-[rgb(250,250,131)] flex justify-between px-5 py-5 ${className}`}
         >
-          <h3 className="text-2xl text-gray-700 font-bold">{title}</h3>
-          <p className="text-sm w-3/4 text-center">{description}</p>
+          <h3 className="text-3xl text-gray-700 font-bold">{title}</h3>
+          <p className="text-base w-3/4 text-center font-serif">{description}</p>
         </div>
       </>
     );

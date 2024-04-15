85c1f60ab5da5d0cf410a92350c26f3bbf92bd0d
kareraolivier <77101006+kareraolivier@users.noreply.github.com>
2024-04-15 07:36:24 -0700

    Ft/block-width-components (#25)
    * feat: refactor the advanced resources and basic resources and case study components
    
    * feat: add good styling
    
    * feat: add case study detail highlight
    
    * chore: cleanup & add seo details
    
    ---------
    
    Co-authored-by: Yannick Musafiri <musayann@gmail.com>

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,10 +2,13 @@ import { Header } from "@/components/header";
 import "@/styles/globals.css";
 import "reactflow/dist/style.css";
 import type { AppProps } from "next/app";
+import { DefaultSeo } from "next-seo"
+import SEO from "../../next-seo.config"
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <Header />
       <Component {...pageProps} />
     </>

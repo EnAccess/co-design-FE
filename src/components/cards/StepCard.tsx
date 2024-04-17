d52b3e4909cf64bd74b91d9c92b79ab7c362410d
Adeline Ayinkamiye <adelinemug6@gmail.com>
2024-04-17 18:01:53 +0200

    feat: add highlight properties to the stepcard

--- src/components/cards/StepCard.tsx
+++ src/components/cards/StepCard.tsx
@@ -1,4 +1,4 @@
-import { useAppContext } from "@/context/app";
+import { useAppContext } from "../../context/app";
 import { useEffect, useState } from "react";
 
 const StepCard = ({ text }: { text: string }) => {
@@ -7,13 +7,13 @@ const StepCard = ({ text }: { text: string }) => {
   const [step, setStep] = useState<string>("");
 
   useEffect(() => {
-    setHighlightedNodes({ attribute: "Step", value: step });
-  })
+    setHighlightedNodes({ attribute: "PROJECT STEP", value: step });
+  },[step])
 
   return (
     <button
       onClick={() => setStep(text)}
-      className="h-32 w-36 bg-[#f8cecc] border rounded-md border-[#b85450] hover:border-purple-500 cursor-pointer flex justify-center items-center"
+      className="h-32 w-36 bg-[#f8cecc] border rounded-lg border-[#b85450] hover:border-2 hover:border-purple-500 cursor-pointer flex justify-center items-center"
     >
       <p className="text-lg text-center font-bold  leading-tight">{text}</p>
     </button>

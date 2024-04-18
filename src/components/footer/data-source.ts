8d1c490fad5de3ae24803245b8415bd724d5fd32
Adeline Ayinkamiye <adelinemug6@gmail.com>
2024-04-18 11:47:08 +0200

    refactor: add the flex on the item

--- src/components/footer/data-source.ts
+++
@@ -1,8 +0,0 @@
-import output from "../../../public/data.json";
-const dataSources = output
-  .map((data) => data?.PARSED_MANUAL_TAGS?.ACCESS)
-  .flat()
-  .filter((source) => source && !source.includes("Open Access"))
-  .filter((source) => source) as string[];
-
-export const uniqueDataSources = Array.from(new Set(dataSources));
--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -1,20 +1,20 @@
 import React from "react";
-import { uniqueDataSources } from "./data-source";
 import Star from "../icons/Star";
 import { getColorByAccess } from "../../utils/helpers";
+import { filteredResources } from "./data-resources";
 
 const Footer = () => {
   return (
-    <footer className="text-center my-4 p-4 px-8 text-base font-bold float-end border-2 border-gray-600 mr-14">
+    <footer className="flex flex-col items-center text-center my-4 p-4 px-8 text-base font-bold float-end border-2 border-gray-600 mr-14">
       <h2 className="mb-4">All Resources are Open Access: unless indicated:</h2>
-      {uniqueDataSources.map((source, index) => {
-        const color = getColorByAccess(source);
+      {filteredResources.map((data, index) => {
+        const color = getColorByAccess(data);
         return (
           <div
             key={index}
-            className="flex gap-5 w-full justify-center items-center"
+            className="flex gap-5 w-2/3 justify-center items-center"
           >
-            <p>{source}</p>
+            <p className="text-center w-full">{data}</p>
             <Star color={color} />
           </div>
         );

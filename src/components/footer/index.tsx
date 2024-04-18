0c63eb57cb7af853240e45ec825971dcd27c4b3d
Adeline Ayinkamiye <adelinemug6@gmail.com>
2024-04-18 13:32:46 +0200

    refactor: remove unneccessary classes

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -5,7 +5,7 @@ import { filteredResources } from "./data-resources";
 
 const Footer = () => {
   return (
-    <footer className="flex flex-col items-center text-center my-4 p-4 px-8 text-base font-bold float-end border-2 border-gray-600 mr-14">
+    <footer className="flex flex-col items-center my-4 p-4 px-8 text-base font-bold float-end border-2 border-gray-600 mr-14">
       <h2 className="mb-4">All Resources are Open Access: unless indicated:</h2>
       {filteredResources.map((data, index) => {
         const color = getColorByAccess(data);

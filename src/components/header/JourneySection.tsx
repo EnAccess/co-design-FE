4ba94b002fd183dd2608bb17a2e12f9f07d2a1e3
Adeline Ayinkamiye <adelinemug6@gmail.com>
2024-04-19 14:21:28 +0200

    feat: add design journey dynamic data

--- src/components/header/JourneySection.tsx
+++ src/components/header/JourneySection.tsx
@@ -1,15 +1,9 @@
 import React from "react";
-import Card from "../cards/StepCard";
+import Card from "../cards/Step";
 import ForwardArrow from "../icons/ForwardArrow";
 import { useHighlightContext } from "@/context/highlight";
+import { filteredJourneyData, steps } from "../../data/design-journey";
 
-const journeys = [
-  "Design",
-  "Stakeholder Mapping",
-  "Needs Assessment",
-  "Implementation",
-  "M&E",
-];
 const JourneySection = () => {
   const { setTag: setHighlightedTag } = useHighlightContext();
   return (
@@ -18,27 +12,30 @@ const JourneySection = () => {
         Select Step in The Co-design Journey
       </p>
       <div className="h-full flex items-center -my-8">
-        <div className="">
-          <div className="flex justify-center items-center my-2">
-            {journeys.map((journey, index) => (
-              <div key={`journey-option-header-${index}`} className="flex items-center gap-2">
-                <Card text={journey} onClick={()=> {
-                  setHighlightedTag(
-                    'PROJECT STEP',
-                    journey
-                  )
-                }} />
-                {index !== journeys.length - 1 && <ForwardArrow />}
+        <div>
+          <div className="flex justify-center my-2">
+            {filteredJourneyData.map((journey, index) => (
+              <div key={index} className="flex items-center gap-2">
+                <Card
+                  text={journey}
+                  onClick={() => {
+                    setHighlightedTag("PROJECT STEP", journey);
+                  }}
+                />
+                {index !== filteredJourneyData.length - 1 && <ForwardArrow />}
               </div>
             ))}
           </div>
 
           <div className="m-2 p-3 bg-gray-400 text-white flex rounded-lg justify-around text-xl  items-center">
-            <p className="text-md font-semibold">Early Concept</p>
-            <p className="text-md font-semibold">{"->"}</p>
-            <p className="text-md font-semibold">Software Development</p>
-            <p className="text-md font-semibold">{"->"}</p>
-            <p className="text-md font-semibold">Action and testing</p>
+            {steps.map((step, index) => (
+              <>
+                <p className="text-md font-semibold " key={index}>
+                  {step.description}
+                </p>
+                {index < steps.length - 1 ? "->" : null}
+              </>
+            ))}
           </div>
         </div>
       </div>

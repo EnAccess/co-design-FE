94523964528265990c4751637e75a6eef36c80d1
kareraolivier <kareraolivier@gmail.com>
2024-04-29 16:55:43 +0200

    feat: add fixed width

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -3,8 +3,13 @@ import { Html, Head, Main, NextScript } from "next/document";
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
-      <body className="max-w-custom mx-auto">
+      <Head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1, maximum-scale=1"
+        />
+      </Head>
+      <body className="min-w-minWidth max-w-maxWidth mx-auto overflow-x-auto">
         <Main />
         <NextScript />
       </body>

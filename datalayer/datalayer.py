1624f988f988e106bdff9248c314ba240d32f991
Daniel Mohns <daniel.mohns@posteo.de>
2024-10-31 11:58:43 +0100

    Fix export script to export all data

--- datalayer/datalayer.py
+++ datalayer/datalayer.py
@@ -9,7 +9,9 @@ ZOTERO_API_KEY = os.environ["ZOTERO_API_KEY"]
 def sync():
     # https://www.zotero.org/groups/4998453/humanitarian_energy_co-design_toolbox/library
     zot = zotero.Zotero("4998453", "group", ZOTERO_API_KEY)
-    items = zot.items()
+
+    # https://pyzotero.readthedocs.io/en/latest/index.html#read-api-methods
+    items = zot.everything(zot.items())
 
     output_file = "../public/zotero_data.json"
 

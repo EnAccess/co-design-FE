a89e2ceca958f2e89be793b6867f8a780e9b5163
delyc <delyce35@gmail.com>
2024-11-05 16:39:33 +0300

    sorting on zotero data

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -3,7 +3,7 @@ import Block from "./Block";
 
 const AdvancedResources = ({
   data,
-  minifiedHeader,
+  minifiedHeader
 }: {
   data: Group;
   minifiedHeader?: boolean;
@@ -11,31 +11,21 @@ const AdvancedResources = ({
   return (
     <>
       <h3 className="text-gray-600 font-semibold text-center text-2xl my-10">
-        {data.title !== "NO GROUP" && data.title}
+        {data.title != "NO GROUP" && data.title}
       </h3>
       <div className="flex w-full">
         <div
           className={`p-4 grid grid-cols-8 grid-flow-col justify-center gap-3 text-sm w-full`}
         >
           {data.blocks.map((block, i) => {
-            const sortedEntries = block.entries.sort((b, a) => {
-              const levelA = a.PARSED_MANUAL_TAGS["CO-DESIGN LEVEL"] || "";
-              const levelB = b.PARSED_MANUAL_TAGS["CO-DESIGN LEVEL"] || "";
-
-              if (levelA < levelB) return -1;
-              if (levelA > levelB) return 1;
-              return 0;
-            });
-
             return (
-              <div key={`${data.title}-${block.title}-${i}`}>
-                <Block
-                  title={block.title}
-                  description={block.description}
-                  entries={sortedEntries} 
-                  minifiedHeader={minifiedHeader}
-                />
-              </div>
+              <Block
+                key={`${data.title}-${block.title}-${i}`}
+                title={block.title}
+                description={block.description}
+                entries={block.entries}
+                minifiedHeader={minifiedHeader}
+              />
             );
           })}
         </div>

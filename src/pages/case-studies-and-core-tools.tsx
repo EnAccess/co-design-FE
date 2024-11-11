120f75bc7dde2575a9342d0f69b19cfe9af02408
delyc <delyce35@gmail.com>
2024-11-11 10:21:22 +0200

    delete developer consoles

--- src/pages/case-studies-and-core-tools.tsx
+++ src/pages/case-studies-and-core-tools.tsx
@@ -9,7 +9,6 @@ const caseStudiesAndCoreTools = () => {
         <HighlightMenuCard />
       </div>
       {groups?.map((data, index) => {
-        console.log("tesss")
         return (
           <Group
             key={`case-studies-and-core-tools-${data.title}-${index}`}

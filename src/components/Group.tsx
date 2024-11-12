a52708f884568baf17cf8114ba1dbbcddeb95945
Delyce Twizeyimana <delyce35@gmail.com>
2024-11-12 11:15:01 +0200

    Fix/advanced resources (#54)
    * sprting
    
    * sorting groups and bolding titles
    
    * delete developer consoles
    
    * delete developer consoles

--- src/components/Group.tsx
+++ src/components/Group.tsx
@@ -19,6 +19,17 @@ const Group = ({
           className={`p-4 grid grid-cols-10 grid-flow-row-dense gap-3 text-sm w-full`}
         >
           {data.blocks.map((block, i) => {
+            // Sort entries based on "CO-DESIGN LEVEL"
+            const sortedEntries = block.entries.sort((b, a) => {
+              const levelA = a.PARSED_MANUAL_TAGS["CO-DESIGN LEVEL"] || "";
+              const levelB = b.PARSED_MANUAL_TAGS["CO-DESIGN LEVEL"] || "";
+
+              // Assuming "CO-DESIGN LEVEL" is alphanumeric, adjust comparison logic accordingly
+              if (levelA < levelB) return -1;
+              if (levelA > levelB) return 1;
+              return 0;
+            });
+
             return (
               <Block
                 key={`${data.title}-${block.title}-${i}`}

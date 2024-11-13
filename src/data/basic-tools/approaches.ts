c1dd9a6cbcbdcae5445212bea5203c162f8ae668
delyc <delyce35@gmail.com>
2024-11-13 09:59:18 +0200

    structuring basic tools and unfiltering

--- src/data/basic-tools/approaches.ts
+++ src/data/basic-tools/approaches.ts
@@ -8,14 +8,18 @@ const rawApproaches = output
   .filter((approach) => approach) as string[];
 
 export const approaches = Array.from(new Set(rawApproaches)).map(
-    (approach: string) => {
-        const data = toolsData[approach.toUpperCase()];
-        const entries = output.filter((entry) => entry?.PARSED_MANUAL_TAGS?.TOOLS?.includes(approach)
-        );
-        return {
-            title: approach,
-            description: data?.description,
-            entries,
-        };
-    }
+  (approach: string) => {
+    const data = toolsData[approach.toUpperCase()];
+    const entries = output
+      .filter((entry) => entry?.PARSED_MANUAL_TAGS?.TOOLS?.includes(approach))
+      .map((entry) => ({
+        ...entry,
+        hasDefaultBackground: true,
+      }));
+    return {
+      title: approach,
+      description: data?.description,
+      entries,
+    };
+  }
 ) as unknown as Block[];
